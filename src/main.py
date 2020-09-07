2aaadec7315d9e725ac61995e2af4401ac920f7d
Gabriel Piñeiro <grpineiro@gmail.com>
2020-09-07 12:26:46 -0300

    Add main.py. Project base completed.

---
+++ src/main.py
@@ -0,0 +1,35 @@
+from os import path
+
+import tcod
+
+def main() -> None:
+    screen_width = 80
+    screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
+    tileset = tcod.tileset.load_tilesheet(
+        path.realpath('assets/dejavu10x10_gs_tc.png'), 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset = tileset,
+        title = 'Roguelike-py',
+        vsync = True
+        
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order='F')
+        while True:
+            root_console.print(x=player_x, y=player_y, string='@')
+
+            context.present(root_console)
+
+            for event in tcod.event.wait():
+                if event.type == 'QUIT':
+                    raise SystemExit()
+
+if __name__ == '__main__':
+    main()
