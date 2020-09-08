c2f6e7786d0d707daadb00b3ef0b17990008180f
Gabriel Piñeiro <grpineiro@gmail.com>
2020-09-08 08:52:23 -0300

    Removed the action logistics of the Engine class.

--- src/engine.py
+++ src/engine.py
@@ -3,7 +3,6 @@ from typing import Set, Iterable, Any
 from tcod.context import Context
 from tcod.console import Console
 
-from actions import EscapeAction, MovimentAction
 from entity import Entity
 from game_map import GameMap
 from input_handlers import EventHandler
@@ -22,12 +21,7 @@ class Engine:
       if action is None:
         continue
 
-      if isinstance(action, MovimentAction):
-        if self.game_map.tiles['walkable'][self.player.x + action.dx, self.player.y + action.dy]:
-          self.player.move(dx=action.dx, dy=action.dy)
-
-      elif isinstance(action, EscapeAction):
-        raise SystemExit()
+      action.perform(self, self.player)
 
   def render(self, console: Console, context: Context) -> None:
     self.game_map.render(console)
