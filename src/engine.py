8e6c812eb7a47ddb19aaf96ee386acf1df34b8ea
Gabriel Piñeiro <grpineiro@gmail.com>
2020-09-08 08:39:12 -0300

    Updated Engine with GameMap modifies.

--- src/engine.py
+++ src/engine.py
@@ -5,12 +5,14 @@ from tcod.console import Console
 
 from actions import EscapeAction, MovimentAction
 from entity import Entity
+from game_map import GameMap
 from input_handlers import EventHandler
 
 class Engine:
-  def __init__(self, entities: Set[Entity], event_handler: EventHandler, player: Entity):
+  def __init__(self, entities: Set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
     self.entities = entities
     self.event_handler = event_handler
+    self.game_map = game_map
     self.player = player
 
   def handler_events(self, events: Iterable[Any]) -> None:
@@ -21,12 +23,15 @@ class Engine:
         continue
 
       if isinstance(action, MovimentAction):
-        self.player.move(dx=action.dx, dy=action.dy)
+        if self.game_map.tiles['walkable'][self.player.x + action.dx, self.player.y + action.dy]:
+          self.player.move(dx=action.dx, dy=action.dy)
 
       elif isinstance(action, EscapeAction):
         raise SystemExit()
 
   def render(self, console: Console, context: Context) -> None:
+    self.game_map.render(console)
+
     for entity in self.entities:
       console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
